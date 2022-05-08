3ef3e67e9c08dec03c604f135ce09b72c5937251
Nathan Campos <nathan@innoveworkshop.com>
2022-05-08 15:39:24 +0100

    Created a project skeleton

---
+++ src/com/innoveworkshop/pickle/example/ExampleUsage.java
@@ -0,0 +1,14 @@
+package com.innoveworkshop.pickle.example;
+
+/**
+ * Briefly shows how to use this library.
+ * 
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+public class ExampleUsage {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
